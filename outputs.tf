63e55cd8833cc43842807c9b91063dff2d6d35e2
Alexander Mancevice <amancevice@cargometrics.com>
2020-03-21 15:56:58 -0400

    output secrets

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,9 @@
-output facebook_secret_arn {
-  description = "facebook SecretsManager secret ARN"
-  value       = aws_secretsmanager_secret.facebook.arn
+output facebook_secret {
+  description = "facebook SecretsManager secret"
+  value       = aws_secretsmanager_secret.facebook
 }
 
-output facebook_secret_name {
-  description = "facebook SecretsManager secret name"
-  value       = aws_secretsmanager_secret.facebook.name
-}
-
-output google_secret_arn {
-  description = "Google service account SecretsManager secret ARN"
-  value       = aws_secretsmanager_secret.google.arn
-}
-
-output google_secret_name {
-  description = "Google service account SecretsManager secret name"
-  value       = aws_secretsmanager_secret.google.name
+output google_secret {
+  description = "Google service account SecretsManager secret"
+  value       = aws_secretsmanager_secret.google
 }
